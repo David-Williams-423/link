906ca8f737ae8c4e75959a407c75a7d62e887e2f
nathancree <ncree@unc.edu>
2023-10-29 02:41:58 -0400

    Merge main into the branch

--- Link/Views/FriendsView.swift
+++ Link/Views/FriendsView.swift
@@ -37,38 +37,35 @@ struct FriendsView: View {
                 VStack {
                     ForEach(vm.friendsList) { friend in
                         VStack {
-                            Button {
-                                .onTapGesture {
+                            HStack {
+                                Image(friend.profilePictureURL ?? "") //ask about profile pics
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 25, height: 25)
+                                    .clipShape(Circle())
+                                
+                                Text("\(friend.firstName) \(friend.lastName)")
+                                    .foregroundStyle(.black)
+                                    .font(.title2)
+                                    .padding(.leading, 10)
+                                
+                                Spacer()
+                                
+                                Image(systemName: friend.status == .online ? "wifi" : "wifi.slash")
+                                    .foregroundColor(friend.status == .online ? .green : .gray)
+                                    .font(.subheadline)
+                                    .padding(.trailing, 20)
+                            }
+                            .frame(minHeight: 75)
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .onTapGesture {
                                 withAnimation {
                                     currentScreen = .link
                                 }
                             }
-                            } label: {
-                                HStack {
-                                    Image(friend.profilePictureURL ?? "") //ask about profile pics
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 25, height: 25)
-                                        .clipShape(Circle())
-                                    
-                                    Text("\(friend.firstName) \(friend.lastName)")
-                                        .foregroundStyle(.black)
-                                        .font(.title2)
-                                        .padding(.leading, 10)
-                                    
-                                    Spacer()
-                                    
-                                    Image(systemName: friend.status == .online ? "wifi" : "wifi.slash")
-                                        .foregroundColor(friend.status == .online ? .green : .gray)
-                                        .font(.subheadline)
-                                        .padding(.trailing, 20)
-                                }
-                                .frame(minHeight: 75)
-                                .background(Color.white)
-                                .cornerRadius(8)
-                                
-                                Divider()
-                            }
+                            
+                            Divider()
                         }
                     }
                 }
