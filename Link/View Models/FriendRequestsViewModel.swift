aeaa3cfefabb316ef784d459e5b22edf3e88d82a
David Williams <davidlwilliams423@gmail.com>
2023-10-29 03:29:35 -0400

    fix

--- Link/View Models/FriendRequestsViewModel.swift
+++ Link/View Models/FriendRequestsViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+@MainActor
 class FriendRequestsViewModel: ObservableObject {
     @Published var friendRequests: [FriendRequest] = []
     private var firebaseService = FireBaseService.shared
