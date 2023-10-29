9fe5a29d38d6e7069c0514b79732439b86c09610
David Williams <davidlwilliams423@gmail.com>
2023-10-29 05:10:03 -0400

    skip login

---
+++ Link/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Link
+//
+//  Created by David Williams on 10/29/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
